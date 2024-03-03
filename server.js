4537bd0f8481b3a03a2bd4ad1be0f65b6fa22d60
Nayan-Kamble53 <nayankamble712@gmail.com>
2024-03-03 19:05:24 +0530

    changes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { http } = require("http");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
 const app = express();
 
 app.use(cors());
 require("dotenv").config();
 
-const httpServer = http.createServer(app);
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: "http://localhost:3000",
 });
